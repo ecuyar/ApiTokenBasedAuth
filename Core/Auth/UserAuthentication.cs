b08b92c2f64959fb8c5a9d1f7ea7489165700de0
Enes Can UYAR <uyar.enescan@gmail.com>
2022-01-24 18:50:14 +0300

    Added auth classes

---
+++ Core/Auth/UserAuthentication.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Auth
+{
+    public class LogInRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LogInResponse
+    {
+        public bool IsSuccess { get; set; }
+        public string ErrorCode { get; set; }
+        public string AccessToken { get; set; }
+    }
+
+    public class UserAuthentication
+    {
+
+    }
+}
